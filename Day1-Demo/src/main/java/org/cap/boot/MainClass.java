45e4249a334d20c28c08ed740f1142c9d34906b1
User <User@Gagan__Deep>
2020-03-26 15:10:19 +0530

    commit All

---
+++ Day1-Demo/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,12 @@
+package org.cap.boot;
+
+import org.cap.model.SuperClass;
+
+public class MainClass
+{
+	public static void main(String args[])
+	{
+		SuperClass obj=new SuperClass();
+		obj.print();
+	}
+}
---
+++ Day1-Demo/src/main/java/org/cap/model/SuperClass.java
@@ -0,0 +1,13 @@
+package org.cap.model;
+
+public class SuperClass
+{
+	int[] arr= {23,45,55,-77,9};
+	
+	public void print()
+	{
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+			
+	}
+}
